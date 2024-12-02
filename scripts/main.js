89622687abc9642c6b4541c660d2aff24d50716c
the-bug-lover <aleksander24sfj@gmail.com>
2024-12-02 09:55:47 +0100

    completed function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,26 @@
-function test() {
+document.getElementById("images").addEventListener("change", () => changeImg());
+
+function changeImg() {
   const option = document.getElementById("images").value;
   const img = document.getElementById("img");
 
-  img.src = `./`;
+  //   console.log(option);
+  switch (option) {
+    case "imgCat":
+      img.src = `./images/cat.jpg`;
+      break;
+    case "imgElf":
+      img.src = `./images/elf.jpg`;
+      break;
+    case "imgDoshin":
+      img.src = `./images/doshin.png`;
+      break;
+    default:
+      img.src = `#`;
+  }
+
+  //   if (option === "imgCat") img.src = `./images/cat.jpg`;
+  //   else if (option === "imgElf") img.src = `./images/elf.jpg`;
+  //   else if (option === "imgDoshin") img.src = `./images/doshin.png`;
+  //   else img.src = "";
 }
